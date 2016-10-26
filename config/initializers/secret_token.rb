b15066671fe858713012992e5aab48c6914b01f4
Renzo Bautista <renzobautista2@gmail.com>
2016-10-26 00:25:23 -0400

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moodtracker::Application.config.secret_key_base = '8fc8546dfcd8f81938036537e3d7d4d24c2d962f27851af02f9cca9c42bb6249306bdd2c957f5a3fe3fe3eb4bc3645904c964abc1ce5ee10601a37fc4f888a23'
