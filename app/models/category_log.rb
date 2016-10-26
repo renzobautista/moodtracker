da4027d0008c01af7580bf3f468302d368fdcdd2
Renzo Bautista <renzobautista2@gmail.com>
2016-10-26 01:06:20 -0400

    created home controller and model relationships?

--- app/models/category_log.rb
+++ app/models/category_log.rb
@@ -1,2 +1,4 @@
 class CategoryLog < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :log
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,4 @@
 class Log < ActiveRecord::Base
+  belongs_to :user
+  has_many :category_logs
 end
