ada59e61a35e85e6b521c81addd0d892949f255c
Renzo Bautista <renzobautista2@gmail.com>
2016-11-02 16:17:00 -0400

    small fix 2

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -16,13 +16,15 @@ class LogsController < ApplicationController
   def update
     @log = current_user.logs.where(date: Date.today)[0]
     @log.mood = params[:log][:mood]
-    for factor_id, score in params[:log][:factor_logs]
-      factor_log = @log.factor_logs.where(factor_id: factor_id)[0]
-      if factor_log == nil
-        factor_log = @log.factor_logs.build(factor_id: factor_id)
+    if params[:log][:factor_logs] != nil
+      for factor_id, score in params[:log][:factor_logs]
+        factor_log = @log.factor_logs.where(factor_id: factor_id)[0]
+        if factor_log == nil
+          factor_log = @log.factor_logs.build(factor_id: factor_id)
+        end
+        factor_log.score = score
+        factor_log.save
       end
-      factor_log.score = score
-      factor_log.save
     end
     @log.save
     redirect_to root_url
