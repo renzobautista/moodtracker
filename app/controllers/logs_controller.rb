d68d2d11b1aa6c9873171b652af6197825e0e12a
Renzo Bautista <renzobautista2@gmail.com>
2016-11-14 19:25:38 -0500

    public logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,13 @@
 class LogsController < ApplicationController
   def create
+    is_public = false
+    if params[:log][:public] == "1"
+      is_public = true
+    end
     @log = Log.new(user_id: current_user.id, date: Date.today)
     @log.mood = params[:log][:mood]
+    @log.summary = params[:log][:summary]
+    @log.public = is_public
     if params[:log][:factor_logs] != nil
       for factor_id, score in params[:log][:factor_logs]
         factor_log = @log.factor_logs.build(factor_id: factor_id)
@@ -14,8 +20,14 @@ class LogsController < ApplicationController
   end
 
   def update
+    is_public = false
+    if params[:log][:public] == "1"
+      is_public = true
+    end
     @log = current_user.logs.where(date: Date.today)[0]
     @log.mood = params[:log][:mood]
+    @log.summary = params[:log][:summary]
+    @log.public = is_public
     if params[:log][:factor_logs] != nil
       for factor_id, score in params[:log][:factor_logs]
         factor_log = @log.factor_logs.where(factor_id: factor_id)[0]
@@ -28,5 +40,9 @@ class LogsController < ApplicationController
     end
     @log.save
     redirect_to root_url
+  end
+
+  def browse
+    @logs = Log.where(public: true).order('date DESC')
   end
 end
