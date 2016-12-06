fe3eb91aac38b0ac8dad2bb33a62a6953121c810
Renzo Bautista <renzobautista2@gmail.com>
2016-12-06 16:46:59 -0500

    fix decimal again

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -10,7 +10,7 @@ class GraphController < ApplicationController
       @logs = @logs.map { |l| { mood: l.mood, date: l.date, factor_log: l.factor_logs.where(factor_id: params[:factor_id])[0] } }
       @logs = @logs.select {|l| l[:factor_log] != nil }
       @correlation = get_correlation(@logs)
-      @slope = (get_slope(@logs) * 100).round / 100
+      @slope = (get_slope(@logs) * 100).round / 100.0
       @graph_title = "Mood and " + Factor.find(params[:factor_id]).question.titleize + " Over Time"
       @is_dual = true
     end
